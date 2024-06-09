8446453add049e69651c51f58adacd4120ec350b
Austin Oakes <aoakes208@gmail.com>
2024-06-09 15:30:17 -0400

    Reformatted the board to account for spacing agains tiles containg values > than 1 char in length

--- 2048.py
+++ 2048.py
@@ -2,7 +2,6 @@ import numpy as np
 
 board = []
 
-
 # Set the initial state of the board. In this case a 4x4 square.
 def SetBoard(board):
     for i in range(0, 4):
@@ -19,16 +18,28 @@ def AddTile(board):
         i = np.random.randint(4)
         j = np.random.randint(4)
 
-    board[i][j] = 2
+    board[i][j] = np.random.choice([2, 4])
+
+# Print the current state of the board
+def PrintBoard(board):
+
+    # For each tile inside each board list...
+    for list in board:
+        for tile in list:
+            # Print out the number of leading characters 
+            leadChars = 4 - len(str(tile))
+            print( "[" + ("*" * leadChars) + str(tile) + "] ", end="")
+
+        print("\n", end="")   
+
+    print()
 
 # Start by setting up the board
 SetBoard(board)
 
-'''
-for i in range(1, 11):
-    AddTile(board)
-'''
+#****************Testing section******************#
 
-# Print the current state of the board for testing purposes
-# Currently set to print each list element (other lists) on a new line via * operatior and sep='\n'
-print(*board, sep='\n')
+PrintBoard(board)
+
+
+ 
